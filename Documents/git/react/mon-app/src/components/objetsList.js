15cf5eedfbd6b1e7acfee3caf7027385dc4d2caa
Doudourgova <94378008+doudourgovamarem@users.noreply.github.com>
2022-03-11 16:13:27 +0100

    ajouts d'iamges

--- Documents/git/react/mon-app/src/components/objetsList.js
+++ Documents/git/react/mon-app/src/components/objetsList.js
@@ -1,11 +1,13 @@
 import '../assets/css/objetlist.css'
 import cv1 from '../assets/image/cv1.png'
 import cv2 from '../assets/image/cv2.png'
+import cv3 from '../assets/image/cv3.png'
+
 
 let objetsList = [
     { name: "CV_1", description: "description 1", price: "4,99€", image: cv1 },
     { name: "CV_2", description: "description 2", price: "4,99€", image: cv2 },
-    { name: "CV_3", description: "description 3", price: "4,99€", image: '' },
+    { name: "CV_3", description: "description 3", price: "4,99€", image: cv3 },
 ]
 
 export default function objetsListfun() {
